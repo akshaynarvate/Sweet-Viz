2aff8419a955cb61771a8dd99b6c91e817bc8ddc
Akshay Narvate <88389553+akshaynarvate@users.noreply.github.com>
2022-07-25 14:57:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def build_model(df):
     st.markdown(filedownload(train_report.show_html("Report.html"), unsafe_allow_html=True)
     
     # Comparing the training and testing dataset using Sweetviz
-    st.write("Comparing Train & Test dataset Sweetviz report")
+                
     st.markdown(filedownload(comparison_report.show_html("Comparison.html"), unsafe_allow_html=True)
     
 # Download html file data
